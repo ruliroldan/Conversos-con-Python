b7fcfef31d83ae5e913b3dbac2593d4a5afb47e7
ruliroldan <ruliroldan@gmail.com>
2024-07-24 11:45:04 -0300

    Agregado archivo mi_archivo.py

---
+++ OneDrive/Escritorio/ahora python/hola.py
@@ -0,0 +1,18 @@
+#PROGRAMA DE PRACTICA PARA EL CURSO DE DOMESTIKA
+
+print("Bienvenidos a mi programa")
+
+print("Escribe un numero en millas")
+
+millas = input()
+
+print("tipo de dato de millas", type(millas))
+millas = float(millas)
+
+print("tipo de dato de millas", type(millas))
+
+kilometros = millas * 1.609
+
+print("Millas ingresadas:", millas)
+print("Kilómetros:", kilometros)
+
